f62e77e85658e6d2a5c993217879a8e03492a49d
Josh <JoshZirena@gmail.com>
2020-06-12 12:51:27 +0200

    refactor: init a different simple node app

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
+const express = require("express");
+const app = express();
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
 
-async function main() {
-  while(true) {
-    console.log("Microservices rock!");
-    console.log(process.env.SAMPLE_ENV);
-    await sleep(5000);
-  }
-}
-
-main();
+app.get("/health", (req, res, next) => {
+  res.status(200).send("Hello!");
+});
